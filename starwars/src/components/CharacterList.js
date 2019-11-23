ee507492159a75a482cef15b29676cf80d95b221
Notesong <notesong.git@outlook.com>
2019-11-22 22:51:34 -0500

    Add weight and page title

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import CharacterCard from './CharacterCard';
 import Buttons from './Buttons';
-import { Container, Row, Button } from "reactstrap";
+import { Container, Row } from "reactstrap";
 
 import './StarWars.css';
 
 export default function Characters() {
     const [characterList, setCharacterList] = useState([]);
+    // page numbers to change current page
     const [pageNumber, setPageNumber] = useState('1');
 
     const didUpdate = () => {
         axios
+            // pull character list from server based on current page number
             .get(`https://swapi.co/api/people/?page=${pageNumber}`)
             .then(response => {
                 setCharacterList(response.data.results);
@@ -20,18 +22,19 @@ export default function Characters() {
             .catch(error => console.log(error))
     }
 
+    // update page when pageNumber is updated
     useEffect(didUpdate, [pageNumber]);
 
     return(
-        <div className='page-container'>
-            <Container className='character-container'>
-                <Buttons setPageNumber={setPageNumber} pageNumber={pageNumber} />
-                <Row>
-                    {characterList.map(character => {
-                        return <CharacterCard character={character} key={character.name} />;
-                    })}
-                </Row>
-            </Container>
-        </div>
+        <Container className='character-container'>
+            {/* add buttons to page */}
+            <Buttons setPageNumber={setPageNumber} pageNumber={pageNumber} />
+            <Row>
+                {/* character list creation */}
+                {characterList.map(character => {
+                    return <CharacterCard character={character} key={character.name} />;
+                })}
+            </Row>
+        </Container>
     );
 }
