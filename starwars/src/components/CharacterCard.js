417e366ca2d8a7c8d3e1177c3e47bd19e956f9f1
Notesong <notesong.git@outlook.com>
2019-11-22 21:32:40 -0500

    CharacterCard complete

---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Film from './Film';
+
+export default function CharacterCard({ character }) {
+    return(
+        <div className='character-card'>
+            <h2>{character.name}</h2>
+            <p>Birth Year: {character.birth_year}</p>
+            <p>Height: {character.height}</p>
+            <p>Eye Color: {character.eye_color}</p>
+            <p>Hair Color{character.hair_color}</p>
+            <p>Films:</p>
+            <ul>
+                {character.films.map(film => {
+                    return <Film film={film} key={film} />;
+                })}
+            </ul>
+        </div>
+    );
+}
