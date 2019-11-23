fd16b0d7b3698cab026e390d1d34c365b90a54b0
Notesong <notesong.git@outlook.com>
2019-11-22 21:25:26 -0500

    Basic file structure complete and axios call added

---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
+
+import './StarWars.css';
+
+export default function Characters() {
+    const [characterList, setCharacterList] = useState([]);
+
+    const didUpdate = () => {
+        axios
+            .get()
+            .then(response => {
+                setCharacterList(response.data.results);
+                console.log(response.data.results);
+            })
+            .catch(error => console.log(error))
+    }
+
+    useEffect(didUpdate, []);
+
+    return(
+        <div className='character-container'>
+            {characterList.map(character => {
+                return <CharacterCard character={character} key={character.name} />;
+            })}
+        </div>
+    );
+}
